16ddd8af3e1cfa84e87bf821b507aebb33a72c27
muhammad aji solehudin <muhammadajisolehudin@gmail.com>
2023-11-03 19:57:22 +0700

    delete folder back-up

--- backup/utils/api-endpoints.jsx
+++
@@ -1,8 +0,0 @@
-export const API_ENDPOINTS = {
-    SEARCH_MOVIE: "/api/v1/search/movie",
-    POPULAR_MOVIE : "/api/v1/movie/popular",
-    DETAIL_MOVIE : "/api/v1/movie/",
-    REGISTER_USER : "/api/v1/auth/register",
-    GET_ME: "/api/v1/auth/me",
-    LOGIN_USER : "/api/v1/auth/login"
-}
